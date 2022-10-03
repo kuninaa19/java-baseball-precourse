18262b830c4f68553a82a45833db538ca9c20e44
kuninaa19 <minchan0527@naver.com>
2022-10-03 14:14:52 +0900

    feat(Game): 게임이 종료된 후 게임을 다시 시작하거나 완전히 종료

--- src/main/java/baseball/model/Game.java
+++ src/main/java/baseball/model/Game.java
@@ -6,17 +6,24 @@ public class Game {
     Computer computer;
     Player player;
     GameView gameView;
+    boolean playing;
 
     public Game(Computer computer, Player player, GameView gameView) {
         this.computer = computer;
         this.player = player;
         this.gameView = gameView;
+        this.playing = true;
     }
 
     public void play() {
-        computer.generateBallNumber();
+        while (playing) {
+            computer.generateBallNumber();
 
-        progress();
+            progress();
+
+            gameView.printProceedWithTheGameGuide();
+            playing = playContinue(player.input());
+        }
     }
 
     public void progress() {
@@ -30,5 +37,13 @@ public class Game {
 
             end = computer.judgmentPlayerBall(player.getBallNumber());
         }
+    }
+
+    private boolean playContinue(int inputNumber) {
+        if (inputNumber != 1 && inputNumber != 2) {
+            throw new IllegalArgumentException("입력값은 1이거나 2를 입력해야합니다. 입력값 : " + inputNumber);
+        }
+
+        return inputNumber == 1;
     }
 }
